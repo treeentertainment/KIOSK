9d3ebef5655407920f8afdadb50feeb1c33ef935
moonlight <160602348+treecorporation@users.noreply.github.com>
2025-05-18 11:36:45 +0900

    Update cart.js

--- V4/cart.js
+++ V4/cart.js
@@ -20,6 +20,8 @@ window.onload = function() {
       const state = snapshot.val();
       if (state && Number(state.state) > 1) {
         window.location.href = "index.html"; // 첫 페이지로 이동
+      } else {
+       selectoption(window.data);
       }
     });
   } catch (error) {
